66aef5ae90f7603f24965a82a6b16f92a0ee09ce
Cesar Coutino <ccoutino@gmail.com>
2024-02-27 11:48:48 -0700

    W02 Learning Activities

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,13 +1,62 @@
 using System;
+using System.Collections.Generic;
 
-public class Program
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
+        //int[] d = R(5);
+        int[] d = {2,2,3,3,3};
+        Array.Sort(d);
+        Console.WriteLine("Values: " + string.Join(", ", d));
+        int s = C(d);
+        Console.WriteLine("Total: " + s);
+    }
 
-        Console.WriteLine("Hello Sandbox World!");
+    static int[] R(int n)
+    {
+        Random r = new Random();
+        int[] d = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            d[i] = r.Next(1, 7);
+        }
+        return d;
+    }
+
+    static int C(int[] d)
+    {
+        int s = 0;
+        Dictionary<int, int> c = new Dictionary<int, int>();
+        foreach (int x in d)
+        {
+            if (c.ContainsKey(x))
+            {
+                c[x]++;
+            }
+            else
+            {
+                c[x] = 1;
+            }
+        }
+        foreach (int v in c.Values)
+        {
+            switch (v)
+            {
+                case 2:
+                    s += 10;
+                    break;
+                case 3:
+                    s += 20;
+                    break;
+                case 4:
+                    s += 30;
+                    break;
+                case 5:
+                    s += 40;
+                    break;
+            }
+        }
+        return s;
     }
 }
--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: The first element to dequeue is in 0 index not 1
 
         Console.WriteLine("------------");
 
@@ -28,7 +28,7 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: The FIFO needs to add elements in the front and move the rest. It should be Add instead of Insert at the front
 
         Console.WriteLine("------------");
 
@@ -54,7 +54,8 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        //_queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +67,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
